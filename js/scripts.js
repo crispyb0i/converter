b1b0dbf02d2e9ce82692bfc4ad13800a71400746
David Shin and laTanya Donaldson <david@>
2017-03-20 14:32:11 -0700

    add gallon and liter converters

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+/*var convertFahrenheightToCelcius = function() {
+return ((parseInt(prompt("Type the fahrenheight that you want to convert to Celcius"))-32)*5/9)
+}
+
+convertFahrenheightToCelcius()
+
+var convertCelciusToFahrenheight = function() {
+return ((parseInt(prompt("Type the celcius that you want to convert fahrenheight"))*9)/5+32)
+
+}
+
+convertCelciusToFahrenheight()*/
+
+var convertGallonsToLiters = function() {
+   alert(parseFloat(prompt("Type in the gallons you want to convert into liters")*3.785411784))
+}
+
+// convertGallonsToLiters();
+
+
+var convertLitersToGallons = function() {
+   alert(parseFloat(prompt("Type in the liters you want to convert into gallons")/3.785411784))
+}
+
+convertLitersToGallons();
